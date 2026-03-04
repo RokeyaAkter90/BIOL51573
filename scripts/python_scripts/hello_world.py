55d1cd4a909d427879a3ae2bc64da52dafd29be5
RokeyaAkter90 <rokeya.39ju@yahoo.com>
2026-03-04 10:31:32 -0600

    added python_scripts

---
+++ scripts/python_scripts/hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# prompt the user to input their name
+name = input("enter your name: ")
+
+## say 'hello world"
+print('hello world')
+
+## print greeting with user name
+print("helo", name)
+
